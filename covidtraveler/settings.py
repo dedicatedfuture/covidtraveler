5d0093e4dc17332edb6af5ea86419b4711647162
William Zerega <zerega85@gmail.com>
2020-09-26 13:26:54 -0400

    reverting to sqlite3 database to get heroku working

--- covidtraveler/settings.py
+++ covidtraveler/settings.py
@@ -76,14 +76,21 @@ WSGI_APPLICATION = 'covidtraveler.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'tom_db',
+#        'USER': 'django',
+#        'PASSWORD': 'django',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tom_db',
-        'USER': 'django',
-        'PASSWORD': 'django',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
