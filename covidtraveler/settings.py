f0d3bfac34a107c3a72819e98104014f2db106f2
William Zerega <zerega85@gmail.com>
2020-09-27 11:51:18 -0400

    changing db back to mysql for production

--- covidtraveler/settings.py
+++ covidtraveler/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'covidtraveler.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'tom_db',
-#        'USER': 'django',
-#        'PASSWORD': 'django',
-#        'HOST': 'localhost',
-#        'PORT': '3306',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tom_db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
