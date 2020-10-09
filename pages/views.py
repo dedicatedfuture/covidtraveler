ee6cb65f1719afa16b5a1acd741fc026ac3fc5aa
William Zerega <zerega85@gmail.com>
2020-10-09 12:14:33 -0400

    added zip code form to home page, added test to validate

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ZipCodeForm
 import feedparser
 
 # Create your views here.
 def index(request):
 	#return HttpResponse("<h1>Hello World!</h1>")
-	return render(request, 'base.html')
+	form = ZipCodeForm()
+	
+	context={'form': form}
+	if request.method == 'POST':
+		print(request.POST)
+		zip_code=ZipCodeForm(request.POST)
+		context = {'zip_code': zip_code}
+		#TO_DO send zip_code to model
+	return render(request, 'base.html', context)
 
 
 def contactus(request):
