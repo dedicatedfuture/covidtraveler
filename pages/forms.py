d7a999d5c78ad9ebe0ec4a14f47dd52b94791e3d
William Zerega <zerega85@gmail.com>
2020-11-05 19:01:27 -0500

    working on updating form

--- pages/forms.py
+++ pages/forms.py
@@ -17,7 +17,13 @@ class StateField(forms.MultiValueField):
 
 class ZipCodeForm(forms.Form):
 	#zipCode = forms.RegexField(label='Zip code')
+
 	zipCode = forms.CharField(label='Zip code')
+
+
+	print("sql from zip code form: ", sql)
+
+	request_data = retrieveDBdata(request,sql)
 	stateChoices = (('PA', 'PA'), ('DE', 'DE'),('CA', 'CA'))
 	stateChoice = forms.ChoiceField(choices = stateChoices, label='State choice: ')
 		
