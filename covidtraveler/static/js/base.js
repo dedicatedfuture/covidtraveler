1142cd0c380cb08cb6f62940b600b5cbd01265e4
ac30boy <tomshannon58@gmail.com>
2020-11-17 18:55:17 -0500

    Another interim views.py check in for county drop down
    The server code has hard coded a response to the ajax call for county data. This will be used by Bill to debug the county drop down that is not being populated with the response from the server.

--- covidtraveler/static/js/base.js
+++ covidtraveler/static/js/base.js
@@ -2,23 +2,25 @@ $(document).ready(function(){
 	console.log('javascript working');
 	var token = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 
-
-
 	$('#id_stateChoice').change(function(){
 		console.log("state choice selected",$('#id_stateChoice option:selected').text());
 		$.ajax({
 			headers: { "X-CSRFToken": token },
 			type:'POST', 
 			url: '/get_county/',
+			dataType: "json",
 			data: {
 				state: $('#id_stateChoice option:selected').text()
 			}, 
 			csrfmiddlewaretoken: '{{ csrf_token }}', 
-			success: function(){
-
+			success: function(result){
+				console.log('results: ', result);
+				var $countyDropDown = $('#id_countyChoice')
+				$.each(result, function() {
+					$countyDropDown.update(result.get_county);
+				});
 			}
 		})
 	});
-	
 
 });
